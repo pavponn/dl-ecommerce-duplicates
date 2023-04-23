895fa89464db0624f5989a63fb24cc4dd3f565a1
pavponn <pavponn@yandex.ru>
2023-04-23 09:28:14 +0100

    Updates for contrastive loss

--- modules/losses/ContrastiveLoss.py
+++ modules/losses/ContrastiveLoss.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class ContrastiveLoss(nn.Module):
@@ -8,14 +9,17 @@ class ContrastiveLoss(nn.Module):
     http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
     """
 
-    def __init__(self, m: float = 1.0, distance: nn.Module = nn.PairwiseDistance()):
+    def __init__(self, m: float = 1.0, distance: nn.Module = nn.PairwiseDistance(), device=torch.device("cpu")):
         super(ContrastiveLoss, self).__init__()
         self.m = m
         self.distance = distance
+        self.device = device
 
     def forward(self, x_1, x_2, y):
-        dist = self.distance(x_1, x_2)
+        x_1 = F.normalize(x_1, p=2, dim=1)
+        x_2 = F.normalize(x_2, p=2, dim=1)
+        dist = self.distance(x_1, x_2).to(self.device)
         return 0.5 * torch.mean(
             (1 - y) * torch.pow(dist, 2) +
-            y * torch.pow(torch.maximum(torch.zeros(dist.size()), self.m - dist), 2)
+            y * torch.pow(torch.maximum(torch.zeros(dist.size()).to(self.device), self.m - dist), 2)
         )
