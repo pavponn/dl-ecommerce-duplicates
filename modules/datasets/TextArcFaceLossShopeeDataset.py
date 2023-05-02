94de79bae01c8f5ae6092e67120aa1aff304b174
Sharath Nataraj <Sharath.Nataraj@ibsplc.com>
2023-05-02 18:02:14 +0100

    more text baseline and losses

--- modules/datasets/TextArcFaceLossShopeeDataset.py
+++ modules/datasets/TextArcFaceLossShopeeDataset.py
@@ -8,14 +8,20 @@ class TextArcFaceLossShopeeDataset(Dataset):
 
     def __len__(self):
         return self.csv.shape[0]
+    
+    def __get__ids__mask(text):        
+        text = self.tokenizer(text, padding='max_length', truncation=True, max_length=64, return_tensors="pt")
+        input_ids = text['input_ids'][0]
+        attention_mask = text['attention_mask'][0] 
+        
+        return input_ids, attention_mask
 
     def __getitem__(self, index):
         row = self.csv.iloc[index]
         
         text = row.title
-        
         text = self.tokenizer(text, padding='max_length', truncation=True, max_length=64, return_tensors="pt")
         input_ids = text['input_ids'][0]
-        attention_mask = text['attention_mask'][0]  
+        attention_mask = text['attention_mask'][0] 
         
-        return input_ids, attention_mask, torch.tensor(row.label_group)
+        return input_ids, attention_mask, torch.tensor(row.label_group) 
